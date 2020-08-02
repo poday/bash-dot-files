94a6cbede5cb79f5738c7c6fdb2ebf2dd16efa48
Patrick ODay <podaydev@gmail.com>
2020-08-02 16:31:19 -0700

    standardizing some process with functions and supporting sym links

--- profile.bash
+++ profile.bash
@@ -2,13 +2,26 @@
 
 #set -x
 
-[[ -d "$HOME/bin:$PATH" ]] && export PATH="$HOME/bin:$PATH";
+function is_bin_in_path {
+    if [[ -n $ZSH_VERSION ]]; then
+        builtin whence -p "$1" &> /dev/null
+    else  # bash:
+        builtin type -P "$1" &> /dev/null
+    fi
+}
 
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+function add_dir_to_path {
+    [[ -d "$1" ]] && export PATH="$1:$PATH";
+}
 
-[[ -f /usr/local/etc/bash_completion ]] && . /usr/local/etc/bash_completion
+function source_existing_file {
+    [[ -f $1 ]] && source $1
+}
 
-export GLICOLOR=1
+function set_script_dir {
+    local script_path="$(readlink -f ${BASH_SOURCE[0]})"
+    SCRIPTDIR="$( cd "$( dirname "${script_path}" )" >/dev/null 2>&1 && pwd )"
+}
 
 function prompt_command {
     # capture the previous command's exit code before it is over writen
@@ -22,26 +35,46 @@ function prompt_command {
 
     export PS1=$($SCRIPTDIR/bash_prompt_command.bash $RET)
 }
+
+add_dir_to_path "$HOME/bin"
+add_dir_to_path "$HOME/.cargo/bin"
+add_dir_to_path "$HOME/.local/bin"
+
+source_existing_file "$HOME/.bashrc"
+
+set_script_dir
+
+source_existing_file "/usr/local/etc/bash_completion"
+
+export GLICOLOR=1
+
+#bash env var to shorten displayed path prompt
 PROMPT_DIRTRIM=3
 export PROMPT_COMMAND=prompt_command
-
 
 # Get color support for 'less'
 export LESS="--RAW-CONTROL-CHARS"
 
-[[ -f $SCRIPTDIR/aliases.bash ]] && . $SCRIPTDIR/aliases.bash
+source_existing_file "$SCRIPTDIR/aliases.bash"
 
 # Use colors for less, man, etc.
-[[ -f $SCRIPTDIR/less-termcap.bash ]] && . $SCRIPTDIR/less-termcap.bash
+source_existing_file "$SCRIPTDIR/less-termcap.bash"
 
 # bash completion for git
-if command -v foo >/dev/null 2>&1; then
-    [[ -f "$(brew --prefix)/etc/bash_completion.d/git-completion.bash" ]] && . $(brew --prefix)/etc/bash_completion.d/git-completion.bash
+if is_bin_in_path brew; then
+    [[ -f "$(brew --prefix)/etc/bash_completion.d/git-completion.bash" ]] && source $(brew --prefix)/etc/bash_completion.d/git-completion.bash
 fi;
 
 # Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
 [ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" $HOME/.ssh/config | grep -v "[?*]" | cut -d " " -f2- | tr ' ' '\n')" scp sftp ssh;
 
-[[ -d "$HOME/.cargo/bin" ]] && export PATH="$HOME/.cargo/bin:$PATH"
 
-[ -e "$HOME/bin/helm" ] && source <($HOME/bin/helm completion bash)
+if is_bin_in_path kubectl; then
+    source <(kubectl completion bash)
+fi;
+
+if is_bin_in_path helm; then
+    source <(helm completion bash)
+fi;
+
+#source_existing_file "$HOME/.bash_local_profile"
