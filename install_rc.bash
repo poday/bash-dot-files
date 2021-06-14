19beb5b5a92fec7d6c040a5cb54e8e4f5c006d7f
Patrick ODay <podaydev@gmail.com>
2021-06-14 00:41:38 -0700

    Moving the logic to rc.bash so that non-login shells work
    This should allow nested shells to use the bash prompt function.
    Also will mean that vscode will not requires '-l' for the terminal.
    
    There is logic to try to avoid recursion in bash_profile but may not work.

---
+++ install_rc.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#set -x
+
+function set_script_dir {
+    local script_path="$(readlink -f ${BASH_SOURCE[0]})"
+    SCRIPTDIR="$( cd "$( dirname "${script_path}" )" >/dev/null 2>&1 && pwd )"
+}
+
+set_script_dir
+ln -s $SCRIPTDIR/rc.bash $HOME/.bashrc
---
+++ rc.bash
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+
+#set -x
+
+function is_bin_in_path {
+    if [[ -n $ZSH_VERSION ]]; then
+        builtin whence -p "$1" &> /dev/null
+    else  # bash:
+        builtin type -P "$1" &> /dev/null
+    fi
+}
+
+function add_dir_to_path {
+    [[ -d "$1" ]] && export PATH="$1:$PATH";
+}
+
+function source_existing_file {
+    [[ -f $1 ]] && source $1
+}
+
+function set_script_dir {
+    local script_path="$(readlink -f ${BASH_SOURCE[0]})"
+    SCRIPTDIR="$( cd "$( dirname "${script_path}" )" >/dev/null 2>&1 && pwd )"
+}
+
+function prompt_command {
+    # capture the previous command's exit code before it is over writen
+    RET=$?
+    #set the tab/window title. Current length is based around MS's "Windows Terminal"
+    if [[ ${#PWD} < 15 ]]; then
+        echo -en "\033]0;$(whoami)@$(hostname)@${PWD}\a"
+    else
+        echo -en "\033]0;$(whoami)@$(hostname)@...${PWD: -14}\a"
+    fi;
+
+    export PS1=$($SCRIPTDIR/bash_prompt_command.bash $RET $SHLVL)
+}
+
+add_dir_to_path "$HOME/bin"
+add_dir_to_path "$HOME/.cargo/bin"
+add_dir_to_path "$HOME/.local/bin"
+
+source_existing_file "$HOME/.bashrc"
+
+set_script_dir
+
+source_existing_file "/usr/local/etc/bash_completion"
+
+export GLICOLOR=1
+
+#bash env var to shorten displayed path prompt
+PROMPT_DIRTRIM=3
+export PROMPT_COMMAND=prompt_command
+
+# Get color support for 'less'
+export LESS="--RAW-CONTROL-CHARS"
+
+source_existing_file "$SCRIPTDIR/aliases.bash"
+
+# Use colors for less, man, etc.
+source_existing_file "$SCRIPTDIR/less-termcap.bash"
+
+# bash completion for git
+if is_bin_in_path brew; then
+    [[ -f "$(brew --prefix)/etc/bash_completion.d/git-completion.bash" ]] && source $(brew --prefix)/etc/bash_completion.d/git-completion.bash
+fi;
+
+# Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
+[ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" $HOME/.ssh/config | grep -v "[?*]" | cut -d " " -f2- | tr ' ' '\n')" scp sftp ssh;
+
+if is_bin_in_path kubectl; then
+    source <(kubectl completion bash)
+fi;
+
+if is_bin_in_path helm; then
+    source <(helm completion bash)
+fi;
+
+if is_bin_in_path pipenv; then
+    source <(pipenv --completion)
+fi;
+
+source_existing_file "$HOME/.bash_local_profile"
+
+# init the ssh-agent
+if [ -z "$SSH_AUTH_SOCK" ]; then
+   # Check for a currently running instance of the agent
+   RUNNING_AGENT="`ps -ax | grep 'ssh-agent -s' | grep -v grep | wc -l | tr -d '[:space:]'`"
+   if [ "$RUNNING_AGENT" = "0" ]; then
+        # Launch a new instance of the agent
+        ssh-agent -s &> $HOME/.ssh/ssh-agent
+   fi;
+   eval `cat $HOME/.ssh/ssh-agent` > /dev/null 2>&1;
+fi;
